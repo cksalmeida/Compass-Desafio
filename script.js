1a47c60e0d3fda4e28ebb0985aeab4fef2c3d8e8
Davi Mathais <100139909+cksalmeida@users.noreply.github.com>
2024-06-06 03:18:29 -0300

    Versão Final

--- script.js
+++ script.js
@@ -19,60 +19,66 @@ document.getElementById('donate-button2').addEventListener('click', function() {
 })
 
 document.getElementById('cancel-button2').addEventListener('click', function() {
-    document.getElementById('modal-form2').close();
+    document.getElementById('modal-form2').close()
 })
 
-const carrosel = document.querySelector('.carrosel-imagens');
-const arrowButton = document.querySelectorAll('.carrosel-container .botao-carrosel');
-const images = document.querySelectorAll('.imagem-carrosel');
+const carrosel = document.querySelector('.carrosel-imagens')
+const arrowButton = document.querySelectorAll('.carrosel-container .botao-carrosel')
+const images = document.querySelectorAll('.imagem-carrosel')
 
-const imagesToShow = 3;
-const totalImages = images.length;
-const imageWidth = carrosel.clientWidth / imagesToShow;
+const imagesToShow = 3
+const totalImages = images.length
+const imageWidth = carrosel.clientWidth / imagesToShow
 
 for (let i = 0; i < imagesToShow; i++) {
-    carrosel.appendChild(images[i].cloneNode(true));
-    carrosel.insertBefore(images[totalImages - 1 - i].cloneNode(true), images[0]);
+    carrosel.appendChild(images[i].cloneNode(true))
+    carrosel.insertBefore(images[totalImages - 1 - i].cloneNode(true), images[0])
 }
 
-let index = imagesToShow; 
+let index = imagesToShow
 
 const updateCarrosel = (instant = false) => {
     carrosel.scrollTo({
         left: index * imageWidth,
         behavior: instant ? 'auto' : 'smooth'
-    });
+    })
 }
 
-updateCarrosel(true);
+updateCarrosel(true)
 
 arrowButton.forEach(btn => {
     btn.addEventListener('click', () => {
         if (btn.id === 'botao-anterior') {
-            index--;
+            index--
             if (index < 0) {
-                index = totalImages;
-                updateCarrosel(true);
-                index--;
+                index = totalImages
+                updateCarrosel(true)
+                index--
             }
         } else {
-            index++;
+            index++
             if (index >= totalImages + imagesToShow) {
-                index = imagesToShow;
-                updateCarrosel(true);
+                index = imagesToShow
+                updateCarrosel(true)
                 index++;
             }
         }
         updateCarrosel();
-    });
-});
+    })
+})
 
 carrosel.addEventListener('scroll', () => {
     if (carrosel.scrollLeft <= 0) {
-        index = totalImages;
-        updateCarrosel(true);
+        index = totalImages
+        updateCarrosel(true)
     } else if (carrosel.scrollLeft >= (totalImages + imagesToShow) * imageWidth) {
-        index = imagesToShow;
-        updateCarrosel(true);
+        index = imagesToShow
+        updateCarrosel(true)
     }
-});
+})
+
+document.getElementById('donate-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    
+    window.location.href = 'success.html'
+})
